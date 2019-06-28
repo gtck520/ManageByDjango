3bc3112297fb7b4c12bf8586592fd26021ccd679
gtck520 <371609974@qq.com>
2019-06-28 16:16:29 +0800

    重构项目目录

--- ManageByDjango/settings.py
+++ ManageByDjango/settings.py
@@ -16,7 +16,7 @@ import datetime
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0,os.path.join(BASE_DIR,'extra_apps'))
-sys.path.insert(0,os.path.join(BASE_DIR,'appone'))
+sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'xadmin',
     'crispy_forms',
     'reversion',
-    'newadmin',  # 此为后台应用
     # 配置api接口开发
     'users',  # 用户api模块
     'rest_framework',
--- users/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyapiConfig(AppConfig):
-    name = 'users'
